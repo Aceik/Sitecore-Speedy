16f086de7e4819cb37bbfb078139de2a5935b592
Thomas Tyack <ttyack@hotmail.com>
2020-10-11 00:45:08 +1100

    WIP

--- src/Foundation/Speedy/code/Scripts/main.js
+++ src/Foundation/Speedy/code/Scripts/main.js
@@ -19,74 +19,117 @@ var processCritical = function (pageUrl, width, height, cb) {
 
     var url = pageUrl;
 
-    logger.debug('about to critical:' + url);
-    logger.log("about to run critical HTML generator " + url);
+    criticalLogger('about to critical:' + url);
+    criticalLogger("about to run critical HTML generator " + url);
 
     try {
 
+        const args = [
+            //'--no-sandbox',
+           // '--disable-dev-shm-usage',
+            '--enable-features=NetworkService',
+            '--ignore-certificate-errors',
+            '--ignore-certificate-errors-spki-list'
+          ];
+          const options = {
+            args,
+            ignoreHTTPSErrors: true,
+            //headless: true,
+          };
+
         critical.generate({
-            src: url,
+            request: { /* got options */ 
+                strictSSL: false,
+                https: { rejectUnauthorized: false }
+            },
+            src: 'http://sc10aceik.australiaeast.cloudapp.azure.com:700/?speedyByPass=true',
+            base: '/',
+            //strict: false,
             minify: true,
-            inline: false,
-            extract: true,
-            pathPrefix: '/',
-            timeout: parseInt(config.timeout),
+            target: {css: 'critical.css' , html: 'index-critical.html' , uncritical: 'uncritical.css'},
+            // rebase: asset => testFunction(`${url}${asset.absolutePath}`),
+            rebase: false,
+            //inline: false,
+            //extract: false,
             width: parseInt(width),
             height: parseInt(height),
             penthouse: {
-                url: url,
-                cssString: ''
+                timeout: parseInt(config.timeout),
+                //cssString: '',
+                //renderWaitTime: 2000,
+                puppeteer: options,
+                //strict: false
             },
         }).then(function (result) {
-            logger.log("critical node tool -> promise resolved");
+            criticalLogger("critical node tool -> promise resolved", result);
 
-            let cleanedUpCcss = new CleanCss({ compress: true }).minify(result).styles;
+            // const cleanuoTool = new CleanCss({ compress: true });
+            // const cleanupREsult = cleanuoTool.minify(result);
+            // let cleanedUpCcss = cleanupREsult.styles;
+            let cleanedUpCcss = result.css;
 
-            logger.debug('html: ' + cleanedUpCcss);
+            criticalLogger('html: ' + cleanedUpCcss);
 
-            logger.log("--------------------------------------------");
-            logger.log("Critical HTML was generated");
-            logger.log("--------------------------------------------");
+            criticalLogger("--------------------------------------------");
+            criticalLogger("Critical HTML was generated");
+            criticalLogger("--------------------------------------------");
             
             cb(cleanedUpCcss);
 
         }).catch(function (err) {
-            logger.log("processing result rejected: " + err);
-            logger.error('critical API issues: ' + err);
+            criticalLogger("processing result rejected: " + err, false);
             cb(null);
         });
 
     } catch (err) {
-        logger.log("rejected" + err);
+        criticalLogger("rejected" + err);
         logger.error('general error:' + err);
         cb(null);
     }
 
 }
 
+
+var testFunction = function a(input) {
+
+    criticalLogger("fontReplacement.find -- " + testFunction);
+
+    return input;
+};
+
+
 var switchFontPaths = function replaceAll(input, fontMaps) {
+    if(!fontMaps)
+        return input;
     var output2 = input;
     fontMaps.forEach(function (fontReplacement) {
-        logger.log("fontReplacement.find -- " + fontReplacement.find + " -> " + fontReplacement.replace);
-        output2 = findReplace(output2, fontReplacement.find, fontReplacement.replace);
+        criticalLogger("fontReplacement.find -- " + fontReplacement.find + " -> " + fontReplacement.replace);
+        if(typeof(fontReplacement) !== 'undefined' && fontReplacement.find && fontReplacement.replace)
+            output2 = findReplace(output2, fontReplacement.find, fontReplacement.replace);
     });
     return output2;
 };
 
 var switchFontFaceNames = function replaceAll(input, fontFaceSwitch) {
+    if(!fontFaceSwitch)
+        return input;
     var output3 = input;
     fontFaceSwitch.forEach(function (fontFaceSwitch) {
-        logger.log("fontFaceSwitch.find -- " + fontFaceSwitch.find + " -> " + fontFaceSwitch.replace);
-        output3 = findReplace(output3, fontFaceSwitch.find, fontFaceSwitch.replace);
+        criticalLogger("fontFaceSwitch.find -- " + fontFaceSwitch.find + " -> " + fontFaceSwitch.replace);
+        if(fontFaceSwitch.find && fontFaceSwitch.replace)
+            output3 = findReplace(output3, fontFaceSwitch.find, fontFaceSwitch.replace);
     });
     return output3;
 };
 
 var removeDuplicates = function removeDups(input, removeDuplicates) {
+    if(!fontFaceSwitch)
+        return input;
     var output2 = input;
     removeDuplicates.forEach(function (fontReplacement) {
-        logger.log("fontReplacement.find -- " + fontReplacement.find);
-        output2 = findReplace(output2, fontReplacement.find, "") + fontReplacement.find;        
+        criticalLogger("fontReplacement.find -- " + fontReplacement.find);
+        if(fontReplacement.find && fontReplacement.replace)
+            output2 = findReplace(output2, fontReplacement.find, "") + fontReplacement.find;        
     });
     return output2;
 };
@@ -99,19 +142,41 @@ var escapeRegExp = function(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
 
+var criticalLogger = function(message, extra) {
+
+    if(extra) {
+        console.log(message, extra);
+        logger.debug(message);
+        return;
+    }
+
+    console.log(message);
+    logger.debug(message);
+}
+
 app.post('/', function(req, res){
 
     try{
         process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
-        logger.debug('Got criticallogger logger - critical started.');
+        criticalLogger('Got criticallogger logger - critical started.');
      
-        logger.debug('url:' + req.body.url);
-        logger.debug('width:' + req.body.width);
-        logger.debug('height:' + req.body.height);
-     
+        criticalLogger('url:' + req.body.url);
+        criticalLogger('width:' + req.body.width);
+        criticalLogger('height:' + req.body.height);
+        criticalLogger("Request ", req);
+
+        req.body.url = findReplace(req.body.url, "habitat.speedy.dev", "habitat.speedy.dev:70");      
+
         processCritical(req.body.url, req.body.width, req.body.height, function(criticalCss){
-     
+            
+            if(!criticalCss) {
+                const notice = "Failure --- "+req.body.url+" --- Critical CSS from this URL is empty or failed to generate";
+                criticalLogger(notice);
+                res.send(notice);
+                return;
+            }
+
             if(req.body.fontMap){
                 criticalCss = switchFontPaths(criticalCss, req.body.fontMap);
             }
@@ -124,10 +189,13 @@ app.post('/', function(req, res){
                 criticalCss = switchFontFaceNames(criticalCss, req.body.fontFaceSwitch);
             }
      
+            criticalLogger("Sending response back to Sitecore");
             res.send(criticalCss);
+            criticalLogger("Completed");
         });
     }
-    catch{
+    catch (err){
+        criticalLogger("API Issue", err);
         res.send("API Issue");
     }
 });
