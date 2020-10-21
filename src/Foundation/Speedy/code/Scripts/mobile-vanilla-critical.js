c271f1cb285c2a08a2ec31f0b361926c28b33e3b
Thomas Tyack <ttyack@hotmail.com>
2020-10-21 23:59:35 +1100

    started putting Loader in place

--- src/Foundation/Speedy/code/Scripts/mobile-vanilla-critical.js
+++ src/Foundation/Speedy/code/Scripts/mobile-vanilla-critical.js
@@ -4,3 +4,22 @@ SpeedyVanilla.Loader.MyModule = {
     myVariable: 1,
     myFunction: function () { return 2; }
 };
+
+//https://github.com/andygup/splash-screen-js/blob/master/index.html
+if (Speedy.isMobile) {
+    console.log('mobile loaders kicking in')
+    setTimeout(function () {
+        // Get a reference to the loader's div
+        var loaderDiv = document.getElementById("loader-wrapper");
+        // When the transition ends remove loader's div from display
+        // so that we can access the map with gestures or clicks
+        if (loaderDiv) {
+            loaderDiv.addEventListener("transitionend", function () {
+                loaderDiv.style.display = "none";
+            }, true);
+            // Kick off the CSS transition
+            loaderDiv.style.opacity = 0.0;
+        }
+    }, 200);
+
+}
